226fe3a40ba88edc93ef6ca995c2a7d1b6d282c7
SamSJackson <samuel.jackson.scotland2@gmail.com>
2022-02-24 20:25:19 +0000

    FORMULAE FOR THAT MAN

--- formula.py
+++ formula.py
@@ -1,8 +1,12 @@
 from person import Person
 from project import Project
 
-def formula_func(h : float, i : float, u : float, p : float, t : float):
-	...
+def element_wise_multiply(l1, l2):
+	return [l1[i] * l2[i] for i in range(len(l1))]
 
-def value_func(project : Project):
-	...
+def formula_func(coefficients : list, project : Project):
+	values = element_wise_multiply(coefficients, [project.score, project.minStartDate, 
+												project.duration, project.averageLevel, 
+												project.num_roles])
+	return (values[0] + values[1]) / (values[2] + values[3] + values[4])
+	
